167d2b06c190b311087d2bb1764a5c883179cf52
Yuruch <fuceqwertyt@gmail.com>
2024-09-10 13:55:48 +0300

    add processing level and technologies

--- scrape/utils.py
+++ scrape/utils.py
@@ -1,4 +1,7 @@
-def proces_salary(salary: str):
+from typing import Tuple
+
+
+def process_salary(salary: str) -> Tuple[str, str]:
     min_salary = "N/A"
     max_salary = "N/A"
 
@@ -18,3 +21,16 @@ def proces_salary(salary: str):
         min_salary, max_salary = tuple(salary.split())
 
     return min_salary, max_salary
+
+
+def process_level(title: str, levels: set) -> str:
+    title_upper = title.upper()
+    matching_levels = {level for level in levels if level in title_upper}
+    return matching_levels.pop() if matching_levels else "N/A"
+
+
+def process_technologies(description: str, technologies: list) -> list:
+    if description != "N/A":
+        found_technologies = [tech for tech in technologies if tech.lower() in description.lower()]
+    else:
+        found_technologies = []
